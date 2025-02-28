b8d2f4669909014bf42a16296bb5e34ef244c99e
superspeeder <andy@delusion.org>
2025-02-28 15:41:05 -0800

    Do changes from class

--- src/scenes/Talking.js
+++ src/scenes/Talking.js
@@ -5,34 +5,34 @@ class Talking extends Phaser.Scene {
 
     init() {
         // dialog constants
-        this.DBOX_X = 0			        // dialog box x-position
-        this.DBOX_Y = 400			    // dialog box y-position
-        this.DBOX_FONT = 'gem_font'	    // dialog box font key
+        this.DBOX_X = 0			         // dialog box x-position
+        this.DBOX_Y = 400			     // dialog box y-position
+        this.DBOX_FONT = 'heycomic_font' // dialog box font key
 
-        this.TEXT_X = 50			    // text w/in dialog box x-position
-        this.TEXT_Y = 445			    // text w/in dialog box y-position
-        this.TEXT_SIZE = 24		        // text font size (in pixels)
-        this.TEXT_MAX_WIDTH = 715	    // max width of text within box
+        this.TEXT_X = 50			     // text w/in dialog box x-position
+        this.TEXT_Y = 445			     // text w/in dialog box y-position
+        this.TEXT_SIZE = 24		         // text font size (in pixels)
+        this.TEXT_MAX_WIDTH = 715	     // max width of text within box
 
-        this.NEXT_TEXT = '[SPACE]'	    // text to display for next prompt
-        this.NEXT_X = 775			    // next text prompt x-position
-        this.NEXT_Y = 574			    // next text prompt y-position
+        this.NEXT_TEXT = '(SPACE)' 	     // text to display for next prompt
+        this.NEXT_X = 775		 	     // next text prompt x-position
+        this.NEXT_Y = 574	 		     // next text prompt y-position
 
-        this.LETTER_TIMER = 10		    // # ms each letter takes to "type" onscreen
+        this.LETTER_TIMER = 10		     // # ms each letter takes to "type" onscreen
 
         // dialog variables
-        this.dialogConvo = 0			// current "conversation"
-        this.dialogLine = 0			    // current line of conversation
-        this.dialogSpeaker = null		// current speaker
-        this.dialogLastSpeaker = null	// last speaker
-        this.dialogTyping = false		// flag to lock player input while text is "typing"
-        this.dialogText = null			// the actual dialog text
-        this.nextText = null			// player prompt text to continue typing
+        this.dialogConvo = 0			 // current "conversation"
+        this.dialogLine = 0			     // current line of conversation
+        this.dialogSpeaker = null		 // current speaker
+        this.dialogLastSpeaker = null	 // last speaker
+        this.dialogTyping = false		 // flag to lock player input while text is "typing"
+        this.dialogText = null			 // the actual dialog text
+        this.nextText = null			 // player prompt text to continue typing
 
         // character variables
-        this.tweenDuration = 500        // character in/out tween duration
+        this.tweenDuration = 500         // character in/out tween duration
 
-        this.OFFSCREEN_X = -500         // x,y coordinates used to place characters offscreen
+        this.OFFSCREEN_X = -500          // x,y coordinates used to place characters offscreen
         this.OFFSCREEN_Y = 1000
     }
 
@@ -53,6 +53,7 @@ class Talking extends Phaser.Scene {
         this.minerva = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'minerva').setOrigin(0, 1)
         this.neptune = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'neptune').setOrigin(0, 1)
         this.jove = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'jove').setOrigin(0, 1)
+        this.crab = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'crab').setOrigin(0, 1).setScale(0.5)
 
         // input
         cursors = this.input.keyboard.createCursorKeys()
--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -16,15 +16,17 @@ class Title extends Phaser.Scene {
         this.load.image('minerva', 'img/minerva.png')
         this.load.image('jove', 'img/jove.png')
         this.load.image('neptune', 'img/neptune.png')
+        this.load.image('crab', 'img/crab.png')
 
         // load bitmap font
         this.load.bitmapFont('gem_font', 'font/gem.png', 'font/gem.xml')
+        this.load.bitmapFont('heycomic_font', 'font/HeyComic.png', 'font/HeyComic.xml')
     }
 
     create() {
         // add title text
-        this.add.bitmapText(centerX, centerY - 32, 'gem_font', 'THE ODYSSEY', 32).setOrigin(0.5)
-        this.add.bitmapText(centerX, centerY, 'gem_font', 'Press SPACE to start', 16).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY - 32, 'heycomic_font', 'THE ODYSSEY', 32).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY, 'heycomic_font', 'Press SPACE to start', 16).setOrigin(0.5)
 
         // create input
         cursors = this.input.keyboard.createCursorKeys()
